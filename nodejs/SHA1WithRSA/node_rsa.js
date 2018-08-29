d64dfe87319a25624146d4679ef8727cf5f0c659
fov42550564 <42550564@qq.com>
2018-08-29 16:28:43 +0800

    update

--- nodejs/SHA1WithRSA/node_rsa.js
+++ nodejs/SHA1WithRSA/node_rsa.js
@@ -1,4 +1,5 @@
 const NodeRSA = require('node-rsa');
+const crypto = require('crypto');
 const fs = require('fs');
 
 const PRIVATE_KEY = fs.readFileSync('./cert/rsa_private.key');
@@ -12,13 +13,29 @@ const privateKey = new NodeRSA(PRIVATE_KEY);
 
 const signature = privateKey.sign(new Buffer(text), 'base64');
 console.log(signature);
-const ret = publicKey.verify(new Buffer(text), new Buffer(signature, 'base64'));
-console.log(ret);
+
+// const sign = crypto.createSign('RSA-SHA256');
+// sign.update(text);
+// const signature = sign.sign(PRIVATE_KEY, 'base64');
+
+// const ret = publicKey.verify(new Buffer(text), new Buffer(signature, 'base64'));
+// console.log(ret);
+
+const verify = crypto.createVerify('RSA-SHA256');
+verify.update(text);
+const result = verify.verify(PUBLIC_KEY, signature, 'base64');
+console.log('verify:', result);
 
 
 // 公钥进行加密
 const encrypted = publicKey.encrypt(new Buffer(text), 'base64');
 console.log(encrypted);
+
+// const encrypted = crypto.publicEncrypt(PUBLIC_KEY, new Buffer(text)).toString('base64');
+// console.log('signed:', encrypted);
 // 私钥进行解密
-const decrypted = privateKey.decrypt(new Buffer(encrypted, 'base64'), 'utf8');
-console.log(decrypted);
+// const decrypted = privateKey.decrypt(new Buffer(encrypted, 'base64'), 'utf8');
+// console.log(decrypted);
+
+const dist = crypto.privateDecrypt(PRIVATE_KEY, new Buffer(encrypted, 'base64')).toString();
+console.log('dist:', dist);
