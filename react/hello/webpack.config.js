d873be48cfe4a7335e0da5489b756a681cb68390
fov42550564 <42550564@qq.com>
2019-11-29 14:34:18 +0800

    添加多入口文件

--- react/hello/webpack.config.js
+++ react/hello/webpack.config.js
@@ -1,12 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+function getHtmlWebpackPlugin(name, title) {
+    return new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        filename: `${name}.html`,
+        title,
+        chunks: [ name ],
+        inject: true,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: false,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        },
+    });
+}
+
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'home.js'), //指定入口文件
+    entry: {
+        home: './home.js',
+        about: './about.js',
+    }, //指定入口文件
     output: {
         path: path.resolve(__dirname, 'dist'), // 输出的路径
-        filename: 'bundle.js'  // 打包后文件
+        filename: '[name].bundle.js'  // 打包后文件
+    },
+    optimization: {
+        minimize: true,
     },
     module: {
         //加载器配置
@@ -24,9 +52,7 @@ module.exports = {
         ]
     },
     plugins: [
-　　 　　new HtmlWebpackPlugin({
-　　　　 　　template: path.resolve(__dirname, 'index.html'),
-　　　　　　 inject: true
-　　　　 })
-　　 ]
+        getHtmlWebpackPlugin('home', '主页'),
+        getHtmlWebpackPlugin('about', '关于页'),
+    ],
 }
